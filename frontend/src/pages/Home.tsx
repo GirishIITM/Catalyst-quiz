4c08128ee0f877a0329134997ce7e1f280051bf5
GirishIITM <23f2005217@ds.study.iitm.ac.in>
2025-06-28 16:46:31 +0530

    feat: Update Home component to include Login and enhance layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import "../styles/home.css";
+import Login from "./Login";
 
 export default function Home() {
   return (
-    <div className="home"></div>
+    <div className="home">home
+    <Login />
+    </div>
   )
 }
