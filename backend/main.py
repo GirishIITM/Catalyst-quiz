cd4081398101181da7b1075e360be818cfcd51da
1Gireesh <23f2005217@ds.study.iitm.ac.in>
2025-06-28 18:36:39 +0530

    added blueprints

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,16 @@
-from flask import Flask, Blueprint 
+from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
-
+from routes import register_routes
 
 app = Flask(__name__)
-CORS(app)
+CORS(
+    app,
+    origin="*",
+    methods=["GET", "POST", "PUT", "DELETE"],
+)
 
+register_routes(app)
 
+if __name__ == "__main__":
+    app.run(debug=True)
